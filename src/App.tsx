07bcefaf3988bcdb6e0ac45a39235cb3ba8980a9
fatemeh <95496800+fatemeh-bd@users.noreply.github.com>
2025-02-21 16:07:47 +0330

    feat: add photo_url to user state and display user image in App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,42 +2,53 @@ import { useEffect, useState } from "react";
 import WebApp from "@twa-dev/sdk"; // اصلاح وارد کردن WebApp به صورت default
 
 const App = () => {
-  const [user, setUser] = useState<{ first_name?: string } | null>(null);
+  const [user, setUser] = useState<{
+    first_name?: string;
+    photo_url?: string;
+  } | null>(null);
 
   useEffect(() => {
     if (WebApp) {
       WebApp.ready();
       WebApp.expand();
-  
+
       console.log("WebApp initDataUnsafe:", WebApp.initDataUnsafe); // لاگ کردن داده‌ها
-  
+
       if (WebApp.initDataUnsafe) {
         if (WebApp.initDataUnsafe.user) {
           setUser(WebApp.initDataUnsafe.user);
         }
       }
-      
+
       document.body.style.backgroundColor = WebApp.themeParams.bg_color;
       // document.body.style.color = WebApp.themeParams.text_color;
     } else {
       console.error("WebApp is not loaded");
     }
   }, []);
-  
+
   return (
     <div
     // style={{
-    //   backgroundColor: WebApp.themeParams.bg_color,  
+    //   backgroundColor: WebApp.themeParams.bg_color,
     //   color: WebApp.themeParams.text_color,
     //   padding: "20px",
     //   textAlign: "center",
     // }}
-  >
-    <h1 style={{color:"red"}}>وب اپ تلگرام</h1>
-    <p>{JSON.stringify(user, null, 2)}</p>
-
-    {user ? <p>سلام {user?.first_name}!</p> : <p>در حال دریافت اطلاعات...</p>}
-  </div>  
+    >
+      <h1 style={{ color: "red" }}>وب اپ تلگرام</h1>
+      <p>{JSON.stringify(user, null, 2)}</p>
+      <img
+        src={user?.photo_url}
+        style={{
+          width: 100,
+          height: 100,
+          borderRadius: 100,
+          objectFit: "cover",
+        }}
+      />
+      {user ? <p>سلام {user?.first_name}!</p> : <p>در حال دریافت اطلاعات...</p>}
+    </div>
   );
 };
 
