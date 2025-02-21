8988378c13021b6080b102eadd4e75082e83a92c
fatemeh <95496800+fatemeh-bd@users.noreply.github.com>
2025-02-21 15:47:20 +0330

    refactor: remove unused CSS and simplify App component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+
 import { useEffect, useState } from "react";
       {/*  @ts-ignore */}
 import { WebApp } from "@twa-dev/sdk";
 
-function App() {
+
+const App = () => {
   const [user, setUser] = useState<{ first_name?: string } | null>(null);
 
   useEffect(() => {
@@ -10,24 +12,23 @@ function App() {
     WebApp.expand();
     setUser(WebApp.initDataUnsafe.user);
     document.body.style.backgroundColor = WebApp.themeParams.bg_color;
-    document.body.style.color = WebApp.themeParams.text_color;
+    // document.body.style.color = WebApp.themeParams.text_color;
   }, []);
   
-  
-
   return (
     <div
-      style={{
-        backgroundColor: WebApp.themeParams.bg_color,
-        color: WebApp.themeParams.text_color,
-        padding: "20px",
-        textAlign: "center",
-      }}
-    >
-      <h1>وب اپ تلگرام</h1>
-      {user ? <p>سلام {user?.first_name}!</p> : <p>در حال دریافت اطلاعات...</p>}
-    </div>
-  );
-}
+    // style={{
+    //   backgroundColor: WebApp.themeParams.bg_color,  
+    //   color: WebApp.themeParams.text_color,
+    //   padding: "20px",
+    //   textAlign: "center",
+    // }}
+  >
+    <h1 style={{color:"red"}}>وب اپ تلگرام</h1>
+    {user ? <p>سلام {user?.first_name}!</p> : <p>در حال دریافت اطلاعات...</p>}
+  </div>  
+  )
+}  
 
-export default App;
+export default App
+
