7da385b31a9e45763e26b4a52327818f63b2d0da
fatemeh <95496800+fatemeh-bd@users.noreply.github.com>
2025-02-21 15:38:31 +0330

    refactor: update user state type and enhance styling in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
 import { useEffect, useState } from "react";
+      {/*  @ts-ignore */}
 import { WebApp } from "@twa-dev/sdk";
 
 function App() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState<{ first_name?: string } | null>(null);
 
   useEffect(() => {
     WebApp.ready();
     WebApp.expand();
     setUser(WebApp.initDataUnsafe.user);
+    document.body.style.backgroundColor = WebApp.themeParams.bg_color;
+    document.body.style.color = WebApp.themeParams.text_color;
   }, []);
+  
+  
 
   return (
-    <div style={{ backgroundColor: WebApp.themeParams.bg_color, color: WebApp.themeParams.text_color, padding: "20px", textAlign: "center" }}>
+    <div
+      style={{
+        backgroundColor: WebApp.themeParams.bg_color,
+        color: WebApp.themeParams.text_color,
+        padding: "20px",
+        textAlign: "center",
+      }}
+    >
       <h1>وب اپ تلگرام</h1>
-      
       {user ? <p>سلام {user?.first_name}!</p> : <p>در حال دریافت اطلاعات...</p>}
     </div>
   );
