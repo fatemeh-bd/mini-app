a18b6c6c89eed6c695efc7d9d55400a419a555ec
Zephinax <zephinax@gmail.com>
2025-03-13 21:54:52 +0330

    add skeleton

---
+++ src/components/Skeleton/Skeleton.tsx
@@ -0,0 +1,11 @@
+const Skeleton = () => {
+    return (
+      <div className="animate-pulse">
+        <div className="h-[56px] bg-gray-200 rounded-lg mb-2"></div>
+        {/* <div className="h-4 bg-gray-200 rounded mb-2"></div>
+        <div className="h-4 bg-gray-200 rounded mb-2"></div> */}
+      </div>
+    );
+  };
+  
+  export default Skeleton;
